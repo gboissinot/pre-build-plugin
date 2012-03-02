0961428a2d79abaa4aa9619923c25f40297190c4
robin-jarry <robin.jarry@ablogix.fr>
2012-03-02 15:20:18 +0100

    Removed the use of a tempFile for the new job creation. Now returns HTTP_BAD_REQUEST codes if parameters or HTTP method are not correct. CreateJobHandler.java returns the job URL when success. BuildHandler.java returns the build URL when success.
    Signed-off-by: robin-jarry <robin.jarry@ablogix.fr>

--- src/main/java/org/jenkinsci/plugins/prebuild/handler/BuildHandler.java
+++ src/main/java/org/jenkinsci/plugins/prebuild/handler/BuildHandler.java
@@ -1,42 +1,73 @@
 package org.jenkinsci.plugins.prebuild.handler;
 
+import hudson.model.TopLevelItem;
 import hudson.model.Hudson;
 import hudson.model.Project;
-import hudson.model.TopLevelItem;
-import org.jenkinsci.plugins.prebuild.PreBuildCause;
-import org.kohsuke.stapler.StaplerRequest;
-import org.kohsuke.stapler.StaplerResponse;
+import hudson.model.Run;
+
+import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
+import org.jenkinsci.plugins.prebuild.PreBuildCause;
+import org.jenkinsci.plugins.prebuild.PreBuildPlugin.BadRequestException;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
 
 /**
  * @author Gregory Boissinot
  */
 public class BuildHandler {
 
-    public void doJobs(StaplerRequest request, StaplerResponse response) throws IOException, ServletException {
-        //Retrieve the existing job
-        String refJobName = request.getRestOfPath();
-        if (refJobName == null) {
-            throw new ServletException("A 'refJobName' is required.");
-        }
-        if (refJobName.startsWith("/")) {
-            refJobName = refJobName.substring(1);
-        }
+    public void doJobs(StaplerRequest request, StaplerResponse response) throws IOException,
+            ServletException
+    {
+        try {
+            // Retrieve the existing job
+            String refJobName = request.getRestOfPath();
+            if (refJobName == null) {
+                throw new BadRequestException("Missing reference 'jobName' in url.");
+            }
+            if (refJobName.startsWith("/")) {
+                refJobName = refJobName.substring(1);
+            }
 
-        TopLevelItem item = Hudson.getInstance().getItem(refJobName);
-        if (item == null) {
-            throw new ServletException(String.format("The given job '%s' doesn't exist.", refJobName));
-        }
-        if (!Project.class.isAssignableFrom(item.getClass())) {
-            throw new ServletException(String.format("The given element '%s' is not a project.", refJobName));
-        }
+            TopLevelItem item = Hudson.getInstance().getItem(refJobName);
+            if (item == null) {
+                throw new BadRequestException(String.format("The job '%s' doesn't exist.",
+                        refJobName));
+            }
+            if (!Project.class.isAssignableFrom(item.getClass())) {
+                throw new BadRequestException(String.format("The element '%s' is not a project.",
+                        refJobName));
+            }
 
-        Project project = (Project) item;
+            Project<?, ?> project = (Project<?, ?>) item;
 
-        project.scheduleBuild(0, new PreBuildCause());
-        response.setStatus(HttpServletResponse.SC_OK);
+            boolean scheduled = project.scheduleBuild(0, new PreBuildCause());
+
+            if (scheduled) {
+                Run<?, ?> build = project.getLastBuild();
+                int checks = 0;
+                while (build == null && checks < 5) {
+                    Thread.sleep(500);
+                    build = project.getLastBuild();
+                    checks++;
+                }
+                response.setContentType("text/plain");
+                response.getWriter().append(build.getUrl());
+                response.setStatus(HttpServletResponse.SC_ACCEPTED);
+            } else {
+                response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+            }
+
+        } catch (BadRequestException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().append(e.toString());
+        } catch (InterruptedException e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            e.printStackTrace(response.getWriter());
+        }
     }
 }
